38ab9489a7d6cd1943770c0fcb36c8f1b4cb3e63
Abu Sayed <rafsansayed132@gmail.com>
2022-09-29 06:25:51 +0600

    added header

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+// import logo from '../../img/icon.webp';
+
+const Header = () => {
+    return (
+        <nav className='header  d-md-flex align-items-center justify-content-md-between '>
+            <div className='d-flex align-items-center justify-content-center text-white header-name '>
+            <p className='h1 me-3 icon '><i class="fa-solid fa-dumbbell"></i></p>
+            <h1><span className='text-info'>POWER-</span><span className='text-danger'>ACTIVE-</span><span className='text-warning'>ORG</span></h1>
+            </div>
+            <div className="d-none d-md-flex header-link">
+                <a href="/home">Home</a>
+                <a href="/login">Login</a>
+                <a href="/blog">Blog</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
